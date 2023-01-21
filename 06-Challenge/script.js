d044cdd261e4bbc55506fdc126ae5a56a1f9c8d5
Ethikry <113566829+Ethikry@users.noreply.github.com>
2023-01-21 05:46:20 -0700

    Added search bar and some api requests

--- 06-Challenge/script.js
+++ 06-Challenge/script.js
@@ -1,12 +1,20 @@
-API key: bef9c29ceef752be853fa2c6c1c0c14d
-var requestLatLonURL = "https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid={bef9c29ceef752be853fa2c6c1c0c14d}"
-var requestCityURL = ""
-
-cityName = document.querySelector("input_city");
+cityName = document.querySelector("#input_city");
 cityIndex = [];
 
+async function searchCity() {
+    cityName = "Toronto";
+    var cityApiSearch = await fetch("http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=5&appid=bef9c29ceef752be853fa2c6c1c0c14d");
+    var cityInfo = await cityApiSearch.json();
 
-function searchCity() {
-    requestCityURL = "http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid={bef9c29ceef752be853fa2c6c1c0c14d}"
-    cityResult = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={bef9c29ceef752be853fa2c6c1c0c14d}";
+    console.log(cityInfo);
+
+    var lat = cityInfo[0].lat;
+    var lon = cityInfo[0].lon;
+
+    var cityWeatherSearch = await fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=bef9c29ceef752be853fa2c6c1c0c14d")
+    var weatherInfo = await cityWeatherSearch.json();
+
+    console.log(weatherInfo);
+    
 }
+searchCity();
